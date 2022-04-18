55ef62ef8a0c256ed8c19bfdef00a1bcc5949fd6
deep1296 <96063026+deep1296@users.noreply.github.com>
2022-04-18 18:15:38 +0530

    products

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/auth");
+    mongoose.connect("mongodb+srv://school:school@cluster0.fprdx.mongodb.net/school_model");
 }
 
 module.exports =connect;
